b53d057e31e2c52cfd9d2ba896cd7077a93235b4
4862abd <4862abd@naver.com>
2021-03-24 12:39:38 +0900

    게시글 등록 추가

---
+++ src/main/java/com/on/nearbnb/board/service/BoardService.java
@@ -0,0 +1,13 @@
+package com.on.nearbnb.board.service;
+
+import java.util.List;
+
+import com.on.nearbnb.board.model.vo.Board;
+
+public interface BoardService {
+	
+	public int insertBoard(Board board);
+//	public int totalCount();
+//	public List<Board> selectList(int startPage, int limit);
+//	public List<Board> selectSearch(String keyword);
+}
